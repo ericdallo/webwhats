d1285b2eadbd026cda56c77527392c0f614b2d3c
Eric Dallo <ericdallo06@hotmail.com>
2022-12-10 11:59:38 -0300

    Improve permissions

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await FlutterDownloader.initialize(debug: false, ignoreSsl: true);
+  await Permission.microphone.request();
+
   runApp(const App());
 }
 
@@ -22,22 +30,63 @@ const desktopUserAgent =
     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36";
 const webWhatsappUrl = "https://web.whatsapp.com/🌎/en/";
 
-class WebwhatsPage extends StatelessWidget {
+class WebwhatsPage extends StatefulWidget {
   const WebwhatsPage({Key? key}) : super(key: key);
 
   @override
+  State<WebwhatsPage> createState() => _WebwhatsPageState();
+}
+
+class _WebwhatsPageState extends State<WebwhatsPage> {
+  InAppWebViewController? _webViewController;
+
+  @override
   Widget build(BuildContext context) {
-    InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
-      crossPlatform: InAppWebViewOptions(
-        userAgent: desktopUserAgent,
-      ),
+    final settings = InAppWebViewSettings(
+      userAgent: desktopUserAgent,
+      allowFileAccessFromFileURLs: true,
+      allowUniversalAccessFromFileURLs: true,
+      useOnDownloadStart: true,
+      allowsInlineMediaPlayback: true,
     );
-    return SafeArea(
-      child: InAppWebView(
-        initialUrlRequest: URLRequest(url: Uri.parse(webWhatsappUrl)),
-        initialOptions: options,
-        // userAgent: desktopUserAgent,
+    final contextMenu = ContextMenu(
+      settings: ContextMenuSettings(hideDefaultSystemContextMenuItems: false),
+    );
+
+    return WillPopScope(
+      child: SafeArea(
+        child: InAppWebView(
+          initialUrlRequest: URLRequest(url: WebUri(webWhatsappUrl)),
+          initialSettings: settings,
+          contextMenu: contextMenu,
+          onWebViewCreated: (InAppWebViewController controller) {
+            _webViewController = controller;
+          },
+          onPermissionRequest: (controller, request) async =>
+              PermissionResponse(
+                  resources: request.resources,
+                  action: PermissionResponseAction.GRANT),
+          onDownloadStartRequest: (controller, url) async {
+            await FlutterDownloader.enqueue(
+              url: url.url.toString(),
+              savedDir: (await getExternalStorageDirectory())!.path,
+              showNotification:
+                  true, // show download progress in status bar (for Android)
+              openFileFromNotification:
+                  true, // click on notification to open downloaded file (for Android)
+            );
+          },
+        ),
       ),
+      onWillPop: () async {
+        if (_webViewController != null) {
+          if (await _webViewController!.canGoBack()) {
+            _webViewController!.goBack();
+          }
+          return false;
+        }
+        return true;
+      },
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_inappwebview
+import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
